4f4cdfcfd0f74bc5e4fb2f906ad09820ecabe7da
Jhonatan Westerhofen dos Santos <61605105+Jhonatan101136@users.noreply.github.com>
2023-03-23 07:05:03 -0300

    Aula de algoritmos avançados
    Removendo e adicionando algoritmos em uma lista dinâmica

--- OrientacaObjetos/src/EmailEnvio/Carteiro.java
+++ OrientacaObjetos/src/EmailEnvio/Carteiro.java
@@ -1,28 +1,28 @@
-package EmailEnvio;
-
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
-
-public class Carteiro {
-	
-	static void enviar(String para, String assunto, String msg) throws EmailException {
-		
-	String username = "jhonatandossantos.inter@outlook.com";
-	String password = "Jhonawest%95";
-
-	Email email = new SimpleEmail();
-	email.setHostName("smtp.office365.com");
-	email.setSmtpPort(587);
-	email.setAuthenticator(new DefaultAuthenticator("jhonatan dos santos", "Jhonawest%95"));
-	email.setFrom("jhonatandossantos.inter@gmail.com");
-	email.setSubject(assunto);
-	email.setMsg(msg);
-	email.addTo(para);
-	email.setStartTLSEnabled(true);
-	email.send();
-
-}
-
-}
+//package EmailEnvio;
+//
+//import org.apache.commons.mail.DefaultAuthenticator;
+//import org.apache.commons.mail.Email;
+//import org.apache.commons.mail.EmailException;
+//import org.apache.commons.mail.SimpleEmail;
+//
+//public class Carteiro {
+//	
+//	static void enviar(String para, String assunto, String msg) throws EmailException {
+//		
+//	String username = "jhonatandossantos.inter@outlook.com";
+//	String password = "Jhonawest%95";
+//
+//	Email email = new SimpleEmail();
+//	email.setHostName("smtp.office365.com");
+//	email.setSmtpPort(587);
+//	email.setAuthenticator(new DefaultAuthenticator("jhonatan dos santos", "Jhonawest%95"));
+//	email.setFrom("jhonatandossantos.inter@gmail.com");
+//	email.setSubject(assunto);
+//	email.setMsg(msg);
+//	email.addTo(para);
+//	email.setStartTLSEnabled(true);
+//	email.send();
+//
+//}
+//
+//}
--- OrientacaObjetos/src/EmailEnvio/UsanoBibliotecaDeEmail.java
+++ OrientacaObjetos/src/EmailEnvio/UsanoBibliotecaDeEmail.java
@@ -1,15 +1,15 @@
-package EmailEnvio;
-
-
-
-public class UsanoBibliotecaDeEmail {
-
-	public static void main(String[] args){
-		
-	Carteiro.enviar("jhonatandossantos.inter@outlook.com", "Teste envio de email", "FUNCIONOOU");
-	
-	System.out.println("deu");
-
-	}
-
-}
+//package EmailEnvio;
+//
+//
+//
+//public class UsanoBibliotecaDeEmail {
+//
+//	public static void main(String[] args){
+//		
+//	Carteiro.enviar("jhonatandossantos.inter@outlook.com", "Teste envio de email", "FUNCIONOOU");
+//	
+//	System.out.println("deu");
+//
+//	}
+//
+//}
