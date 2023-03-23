58eae8136b052875c4134baf0034f26b65e4da06
Jhonatan Westerhofen dos Santos <61605105+Jhonatan101136@users.noreply.github.com>
2023-03-23 06:04:29 -0300

    Email
    Commit throws

--- OrientacaObjetos/src/EmailEnvio/UsanoBibliotecaDeEmail.java
+++ OrientacaObjetos/src/EmailEnvio/UsanoBibliotecaDeEmail.java
@@ -1,10 +1,10 @@
 package EmailEnvio;
 
-import org.apache.commons.mail.EmailException;
+
 
 public class UsanoBibliotecaDeEmail {
 
-	public static void main(String[] args) throws EmailException {
+	public static void main(String[] args){
 		
 	Carteiro.enviar("jhonatandossantos.inter@outlook.com", "Teste envio de email", "FUNCIONOOU");
 	
